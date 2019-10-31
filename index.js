8d207a84614f04ac36787d5a84f88687c0d0357c
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-31 14:52:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,19 +7,19 @@ function BoardMember (name,homeState,training){
     return "No, I must disagree"
   }
   
-    function approve (){
+    BoardMember.prototype.approve=function  (){
     return "You can do that!"
   }
   
-    function doCharity  (){
+    BoardMember.prototype.doCharity=function   (){
     return "I like to help people."
   }
   
-      function releasePressStatement (){
+      BoardMember.prototype.releasePressStatement=function  (){
     return "You will see great things from Scuber."
   }
   
-      function sayHi  (){
+      BoardMember.prototype.sayHi=function   (){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
